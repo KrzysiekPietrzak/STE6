db563a7107a2fb6239fbd9cc598768e8595e2644
KrzysiekPietrzak <Krzysztof.pietrzak17@gmail.com>
2023-04-24 21:09:42 +0200

    video 10

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,24 +7,27 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import HomePage from './pages/HomePage.tsx'
-import ProductPage from './pages/ProductPage.tsx'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import axios from 'axios'
 
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-   <Route index={true} element={<HomePage />}/>
-   <Route path="product/:slug" element={<ProductPage />} />
-
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
     </Route>
   )
 )
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
